f5ca7d7f7ff90a4413f0015f23af01e172c264c3
manik1848 <manikprakash1848@gmail.com>
2023-12-06 11:25:47 +0530

    userDetail api added

--- app.js
+++ app.js
@@ -6,10 +6,16 @@ require('dotenv').config()
 const app = express();
 app.use(express.json())
 app.get('/', async(req, res) => {
-    // res.send('Homepage')
     const users= await UserModel.find();
     console.log(users,'users')
    res.send({data:users})
+})
+
+app.get('/userDetail', async(req, res) => {
+    const {id}=req.query;
+    const user= await UserModel.find({_id:id});
+    console.log(user,'user')
+    res.send({data:user})
 })
 
 app.post('/user',async(req,res)=>{
@@ -17,7 +23,6 @@ app.post('/user',async(req,res)=>{
     const userData = new UserModel(req.body);
     userData.save();
     return res.send({ Message: "User Added succesful" });
-
 })
 
 app.listen(8080, async() => {
